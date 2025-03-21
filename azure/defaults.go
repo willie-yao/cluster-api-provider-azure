79088a65987ce6cbc96ed5b4cb79bea2258b5cf8
willie-yao <williamyao@microsoft.com>
2025-03-21 23:05:21 +0000

    Replace bootstrap extension with custom script extension

--- azure/defaults.go
+++ azure/defaults.go
@@ -26,7 +26,6 @@ import (
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore/arm"
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore/cloud"
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore/policy"
-	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/compute/armcompute/v5"
 	"github.com/Azure/azure-sdk-for-go/sdk/tracing/azotel"
 
 	"sigs.k8s.io/cluster-api-provider-azure/pkg/ot"
@@ -65,9 +64,9 @@ const (
 
 const (
 	// BootstrappingExtensionLinux is the name of the Linux CAPZ bootstrapping VM extension.
-	BootstrappingExtensionLinux = "CAPZ.Linux.Bootstrapping"
+	BootstrappingExtensionLinux = "CustomScript"
 	// BootstrappingExtensionWindows is the name of the Windows CAPZ bootstrapping VM extension.
-	BootstrappingExtensionWindows = "CAPZ.Windows.Bootstrapping"
+	BootstrappingExtensionWindows = "CustomScriptExtension"
 )
 
 const (
@@ -318,19 +317,11 @@ func GetBootstrappingVMExtension(osType string, cloud string, vmName string, cpu
 	// currently, the bootstrap extension is only available in AzurePublicCloud.
 	if osType == LinuxOS && cloud == PublicCloudName {
 		// The command checks for the existence of the bootstrapSentinelFile on the machine, with retries and sleep between retries.
-		// We set the version to 1.1 (will target 1.1.1) for arm64 machines and 1.0 for x64. This is due to a known issue with newer versions of
-		// Go on Ubuntu 20.04. The issue is being tracked here: https://github.com/golang/go/issues/58550
-		// TODO: Remove this once the issue is fixed, or when Ubuntu 20.04 is no longer supported.
-		// We are using 1.1 instead of 1.1.1 for Arm64 as AzureAPI do not allow us to specify the full version.
-		extensionVersion := "1.0"
-		if cpuArchitectureType == string(armcompute.ArchitectureTypesArm64) {
-			extensionVersion = "1.1"
-		}
 		return &ExtensionSpec{
 			Name:      BootstrappingExtensionLinux,
 			VMName:    vmName,
-			Publisher: "Microsoft.Azure.ContainerUpstream",
-			Version:   extensionVersion,
+			Publisher: "Microsoft.Azure.Extensions",
+			Version:   "2.0",
 			ProtectedSettings: map[string]string{
 				"commandToExecute": LinuxBootstrapExtensionCommand,
 			},
@@ -341,8 +332,8 @@ func GetBootstrappingVMExtension(osType string, cloud string, vmName string, cpu
 		return &ExtensionSpec{
 			Name:      BootstrappingExtensionWindows,
 			VMName:    vmName,
-			Publisher: "Microsoft.Azure.ContainerUpstream",
-			Version:   "1.0",
+			Publisher: "Microsoft.Compute",
+			Version:   "1.10",
 			ProtectedSettings: map[string]string{
 				"commandToExecute": WindowsBootstrapExtensionCommand,
 			},
